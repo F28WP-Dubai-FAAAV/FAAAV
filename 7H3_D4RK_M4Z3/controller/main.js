4d82b6e90bb7483f1a8f58139e491b0a5a36e89b
Abdul Kader <an140@hw.ac.uk>
2020-10-25 19:56:54 +0400

    store players in an array

--- 7H3_D4RK_M4Z3/controller/main.js
+++ 7H3_D4RK_M4Z3/controller/main.js
@@ -1,3 +1,9 @@
 import Player from './Player.js'
 
-const player = new Player('AbdulKader',1)
+const canvas = document.getElementById("canvas")
+
+const players = []
+players.push(new Player('AbdulKader',players.length+1))
+
+const img = document.querySelector(".hero")
+img.src = players[0].sprite;
