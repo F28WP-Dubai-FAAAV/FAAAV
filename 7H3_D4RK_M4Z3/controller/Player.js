ee45a4454f96d8e7d42c595e06beb2923d7b9bf3
Abdul Kader <an140@hw.ac.uk>
2020-10-25 17:41:14 +0400

    added a variable to store the location of sprite

--- 7H3_D4RK_M4Z3/controller/Player.js
+++ 7H3_D4RK_M4Z3/controller/Player.js
@@ -2,9 +2,9 @@ class Player{
     topPos = 0;
     leftPos = 0;
     step = 3;
-
-    constructor(username){
+    constructor(username, num){
         this.username = username;
+        this.sprite = `../Assets/players/male_${num}.png`;
     }
 }
 
