dc4fb1e519d232b998d24c52d80a76af61050c9b
Abdul Kader <an140@hw.ac.uk>
2020-10-27 14:05:16 +0400

    added the maze and eventListener for the keys.

--- 7H3_D4RK_M4Z3/controller/main.js
+++ 7H3_D4RK_M4Z3/controller/main.js
@@ -1,5 +1,14 @@
 import Player from './Player.js'
+//Selecting the div with class maze
+const maze = document.querySelector(".maze")
 
 const players = []
 players.push(new Player('AbdulKader',players.length+1))
-players[0].createPlayer(0,0);
+players[0].createPlayer(maze,0,0);
+
+document.addEventListener("keydown",(e)=>{
+    players[0].movementHandler(e,true)
+});
+document.addEventListener("keyup", (e)=>{
+    players[0].movementHandler(e,false)
+})
