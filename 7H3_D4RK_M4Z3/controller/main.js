574acdd86701dd6419878bb41ce42218177b4d9d
Abdul Kader <an140@hw.ac.uk>
2020-10-27 16:03:29 +0400

    controller to move the player & a renderer added.

--- 7H3_D4RK_M4Z3/controller/main.js
+++ 7H3_D4RK_M4Z3/controller/main.js
@@ -1,14 +1,39 @@
 import Player from './Player.js'
+import Movement from './Movement.js'
 //Selecting the div with class maze
 const maze = document.querySelector(".maze")
 
 const players = []
 players.push(new Player('AbdulKader',players.length+1))
-players[0].createPlayer(maze,0,0);
+players[0].createPlayer(maze,400,400);
+
+const controller = new Movement(players[0])
+
+// Code from Frank's Laboratory
+let fps, fpsInterval, startTime, now, then, elapsed;
+fps = 60;
+function startAnimate(fps){
+  fpsInterval = 1000/fps
+  then = Date.now()
+  startTime = then
+  update()
+}
+
+function update(){
+  requestAnimationFrame(update)
+  now = Date.now()
+  elapsed = now - then
+  if(elapsed > fpsInterval){
+    then = now - (elapsed % fpsInterval)
+    players[0].animate()
+  }
+}
+
+startAnimate(fps)
 
 document.addEventListener("keydown",(e)=>{
-    players[0].movementHandler(e,true)
+    controller.movementHandler(e,true)
 });
 document.addEventListener("keyup", (e)=>{
-    players[0].movementHandler(e,false)
+    controller.movementHandler(e,false)
 })
