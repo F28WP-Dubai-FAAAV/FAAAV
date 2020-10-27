a7c953e8b23fa3f6e8ec9e1037aad8793c327c78
Abdul Kader <an140@hw.ac.uk>
2020-10-27 13:02:55 +0400

    removed canvas and img, called createPlayer

--- 7H3_D4RK_M4Z3/controller/main.js
+++ 7H3_D4RK_M4Z3/controller/main.js
@@ -1,9 +1,5 @@
 import Player from './Player.js'
 
-const canvas = document.getElementById("canvas")
-
 const players = []
 players.push(new Player('AbdulKader',players.length+1))
-
-const img = document.querySelector(".hero")
-img.src = players[0].sprite;
+players[0].createPlayer(0,0);
