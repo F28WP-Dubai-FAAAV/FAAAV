7721c0da39ef556e72fd20f666a8b1f3ea54a228
Abdul Kader <an140@hw.ac.uk>
2020-10-27 12:06:01 +0400

    made named methods for the addEventListener

--- 7H3_D4RK_M4Z3/controller/register.js
+++ 7H3_D4RK_M4Z3/controller/register.js
@@ -6,21 +6,25 @@ const formContainer = document.querySelector(".form-container")
 const loginForm = formContainer.children[0]
 const signupForm = formContainer.children[1]
 
-
-loginSwitch.addEventListener("click", ()=>{
+function loginSwitcher(){
     if(!loginSwitch.classList.contains("active")){
         loginSwitch.classList.add("active")
         loginForm.classList.remove("hide")
         signupSwitch.classList.remove("active")
         signupForm.classList.add("hide")
     }
-})
+}
 
-signupSwitch.addEventListener("click", ()=>{
+function signupSwitcher(){
+    console.log("clikc")
     if(!signupSwitch.classList.contains("active")){
         signupSwitch.classList.add("active")
         signupForm.classList.remove("hide")
         loginSwitch.classList.remove("active")
         loginForm.classList.add("hide")
     }
-})
+}
+
+loginSwitch.addEventListener("click", loginSwitcher)
+
+signupSwitch.addEventListener("click", signupSwitcher)
