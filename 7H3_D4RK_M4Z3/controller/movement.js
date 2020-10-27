c8b1df5f316a538fc9ffd81c9f37e509ce05c226
Abdul Kader <an140@hw.ac.uk>
2020-10-27 22:56:22 +0400

    triggering movment animation when arrowkey pressed

--- 7H3_D4RK_M4Z3/controller/movement.js
+++ 7H3_D4RK_M4Z3/controller/movement.js
@@ -23,7 +23,9 @@ function Movement(player){
     // To handle the movement
     this.movementHandler=(e, isKeyDown)=>{
         const keyCode = e.keyCode;
+        // Checks if the button pressed is one of the arrowkeys
         if(keyCode in this.keys){
+            // Checks if it is a keydown or keyup event
             if(isKeyDown){
                 e.preventDefault();
                 this.startMovement(this.keys[keyCode]);
@@ -35,15 +37,21 @@ function Movement(player){
     };
 
     this.startMovement=(key)=>{
-        //TODO : start the player moving animation
+        // Checks if the key pressed is in array keysHeld
         if(!this.keysHeld.includes(key)){
+            // adds the key to the beginning of the array
             this.keysHeld.unshift(key);
         }
+        // variable to store the last key that was pressed
         const topKeyHeld = this.keysHeld[0];
 
         if(topKeyHeld){
             const sheet = this.player.playerDiv.querySelector(".sheet");
+            // Changes the direction the sprite is facing
             sheet.setAttribute("facing", topKeyHeld)
+            // Starts the player movinng animation
+            this.player.playerDiv.querySelector(".player-sprite").setAttribute("moving", "true")
+            // Changes the topPos and leftPos according to the key pressed
             switch(topKeyHeld){
                 case 'up':
                     this.player.topPos -= this.player.speed;
@@ -63,8 +71,13 @@ function Movement(player){
     };
 
     this.stopMovement = (key)=> {
-        //TODO : stop the player moving animation
+        // removes the key which is released from the array keysHeld
         this.keysHeld.splice(this.keysHeld.indexOf(key),1);
+        //Checks if no keys are pressed
+        if(this.keysHeld.length <= 0){
+            // stops the player movinng animation
+            this.player.playerDiv.querySelector(".player-sprite").setAttribute("moving", "false");
+        }
     };
 }
 
