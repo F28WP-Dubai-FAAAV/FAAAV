0fe241b5040998e428779dd2788e1f24e6c6d4ed
Abdul Kader <an140@hw.ac.uk>
2020-10-29 15:38:41 +0400

    added Maze generator object to create a maze

--- 7H3_D4RK_M4Z3/controller/main.js
+++ 7H3_D4RK_M4Z3/controller/main.js
@@ -1,17 +1,28 @@
 import Player from './Player.js'
 import Movement from './Movement.js'
+import MazeGenerator from "./MazeGenerator.js"
 //Selecting the div with class maze
 const maze = document.querySelector(".maze")
 
+const mazeGen = new MazeGenerator(maze);
+mazeGen.createMaze(mazeGen.maze1);
+
+// array to store all the players
 const players = []
+//Pushing new players in the array
 players.push(new Player('AbdulKader',players.length+1))
+//creating the player and displaying it on the screen
 players[0].createPlayer(maze,400,400);
 
+//controller to control a players movement
 const controller = new Movement(players[0])
 
-// Code from Frank's Laboratory
+// Creating a way to render on the screen
 let fps, fpsInterval, startTime, now, then, elapsed;
+// initializing the fps the game will run at
 fps = 60;
+
+// starts the the game
 function startAnimate(fps){
   fpsInterval = 1000/fps
   then = Date.now()
@@ -19,6 +30,7 @@ function startAnimate(fps){
   update()
 }
 
+// updates the game every 1000/60 seconds
 function update(){
   requestAnimationFrame(update)
   now = Date.now()
@@ -31,6 +43,7 @@ function update(){
 
 startAnimate(fps)
 
+// Listenes for keydown and keyup events
 document.addEventListener("keydown",(e)=>{
     controller.movementHandler(e,true)
 });
