6f08fcc17d3d8ca914ba10b109f40cd04267b31c
Abdul Kader <an140@hw.ac.uk>
2020-10-30 13:48:18 +0400

    maze created using top&left, layer two removed

--- 7H3_D4RK_M4Z3/controller/MazeGenerator.js
+++ 7H3_D4RK_M4Z3/controller/MazeGenerator.js
@@ -12,18 +12,7 @@ function MazeGenerator(maze){
             ['030','031','032','033','032','033','032','033','034','035'],
             ['040','041','042','043','042','043','042','043','044','045'],
         ],
-        LayerTwo : [
-            ['-00','-00','-00','-00','-00','-00','-00','-00','-00','-00'],
-            ['-00','-00','100','-00','-00','-00','100','-00','-00','-00'],
-            ['-00','-00','102','100','102','102','100','-00','102','-00'],
-            ['-00','102','102','102','-00','-00','100','103','-00','-00'],
-            ['-00','-00','-00','-00','102','102','-00','103','103','-00'],
-            ['-00','102','102','-00','-00','100','-00','-00','-00','-00'],
-            ['-00','-00','-00','100','102','102','100','102','-00','-00'],
-            ['-00','102','101','100','-00','-00','100','-00','102','-00'],
-            ['-00','-00','-00','100','-00','100','-00','-00','-00','-00'],
-            ['-00','-00','-00','-00','-00','-00','-00','-00','-00','-00'],
-        ]
+        LayerTwo : []
     }
 
     this.createMaze = (mazeMap)=>{
@@ -33,30 +22,20 @@ function MazeGenerator(maze){
         const mazeLayerTwo = document.createElement("div")
         mazeLayerTwo.classList.add("maze-layer-two" )
         maze.append(mazeLayerTwo)
-        mazeMap.LayerOne.forEach((tileRow) =>{
-            tileRow.forEach(tileColumn=>{
+        const tileSize = 80;
+        mazeMap.LayerOne.forEach((tileRow, row) =>{
+            tileRow.forEach((tileColumn, column)=>{
                 let img = `tile${tileColumn}.png`;
                 const imgTile = document.createElement('div');
                 imgTile.classList.add("tile", "pixelart");
                 imgTile.style.background = `url(../Assets/Dungeon_SpriteSheet_Split/${img}) no-repeat center`;
-                imgTile.style.backgroundSize = "cover"; 
+                imgTile.style.backgroundSize = "cover";
+                imgTile.style.top = `${tileSize*row}px`
+                imgTile.style.left = `${tileSize*column}px`
                 mazeLayerOne.appendChild(imgTile);
             })
         })
 
-        mazeMap.LayerTwo.forEach((tileRow) =>{
-            tileRow.forEach(tileColumn=>{
-                let img = `transparent.png`
-                if(tileColumn !== "-00"){
-                    img = `tile${tileColumn}.png`;
-                }
-                const imgTile = document.createElement('div');
-                imgTile.classList.add("wall", "tile", "pixelart");
-                imgTile.style.background = `url(../Assets/Dungeon_SpriteSheet_Split/${img}) no-repeat center`;
-                imgTile.style.backgroundSize = "cover"; 
-                mazeLayerTwo.appendChild(imgTile);
-            })
-        })
     }
 }
 
