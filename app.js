80c6c589c2141a9f9c2083bcd995189977cbb9c7
Faizzann <71325258+Faizzann@users.noreply.github.com>
2020-10-30 14:15:22 +0400

    Add files via upload
    schema partially created.

---
+++ app.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const User = require('./models/users');
+
+//connect to mongodb
+const dbURI = 'mongodb+srv://fhp1:J@m@lpur1@cluster0.yl7ru.mongodb.net/the_dark_maze?retryWrites=true&w=majority';
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true})
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err));
+
+
+// express app
+const app = express();
+app.use(morgan('tiny'));
+
+// mongoose and mango sandbox routes
+app.get('/add-user', (req, res) => {
+  const user = new User({
+    userName: 'faizzan',
+    name: 'Faizan',
+    email_id: 'fhp1@hw.ac.uk'
+  });
+
+  user.save()
+    .then((result) =>{
+      res.send(result)
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.get('/', (req, res) => {
+  res.sendFile('./views/index.html', {root: __dirname});
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile('./views/login.html', {root: __dirname});
+});
+
+// 404 page
+app.use((req, res) => {
+    res.status(404).sendFile('./views/404.html', {root: __dirname});
+
+});
---
+++ models/users.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    userName: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    email_id: {
+        type: String,
+        required: true
+    }
+},{timestamps: true});
+
+const User = mongoose.model('User', userSchema);
+module.export = User;
