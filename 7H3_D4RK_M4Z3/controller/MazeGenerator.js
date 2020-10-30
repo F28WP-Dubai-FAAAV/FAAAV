274bc23ef2b0ac7951317eae904cbfbf4eb5f45d
Abdul Kader <an140@hw.ac.uk>
2020-10-30 19:34:17 +0400

    new method to create layertwo for the maze
    created an array layertwo which stores arrays [x,y,orientation] with info abput its x and y position on the maze and wether if it is vertical/horizontal

--- 7H3_D4RK_M4Z3/controller/MazeGenerator.js
+++ 7H3_D4RK_M4Z3/controller/MazeGenerator.js
@@ -12,7 +12,23 @@ function MazeGenerator(maze){
             ['030','031','032','033','032','033','032','033','034','035'],
             ['040','041','042','043','042','043','042','043','044','045'],
         ],
-        LayerTwo : []
+        LayerTwo : [
+            [140,160,'h'],[380,160,'h'],[460,160,'h'],
+            [80,240,'h'],[160,240,'h'],[240,240,'h'],[400,240,'h'],
+            [160,320,'h'],[240,320,'h'],[480,320,'h'],[560,320,'h'],
+            [160,400,'h'],[240,400,'h'],[320,400,'h'],[480,400,'h'],[560,400,'h'],[640,400,'h'],
+            [480,480,'h'],[560,480,'h'],
+            [560,560,'h'],[640,560,'h'],
+            [400,640,'h'],[480,640,'h'],[560,640,'h'],
+            [220,80,'v'],[540,80,'v'],
+            [300,160,'v'],[540,160,'v'],[620,160,'v'],
+            [380,240,'v'],[460,240,'v'],[620,240,'v'],
+            [140,320,'v'],[380,320,'v'],
+            [220,400,'v'],[380,400,'v'],
+            [140,480,'v'], [220,480,'v'], [300,480,'v'],[380,480,'v'], [540,480,'v'],
+            [140,560,'v'], [220,560,'v'], [300,560,'v'], [460,560,'v'],
+            [140,640,'v'], [300,640,'v'], [460,640,'v'],
+        ]
     }
 
     this.createMaze = (mazeMap)=>{
@@ -36,6 +52,23 @@ function MazeGenerator(maze){
             })
         })
 
+        mazeMap.LayerTwo.forEach((wall) =>{
+            let [x,y,orientation] = wall;
+            let img = `wall${orientation}.png`;
+            const imgTile = document.createElement('div');
+            imgTile.classList.add("wall", "pixelart");
+            if(orientation === 'h'){
+                imgTile.classList.add('horizontal')
+            }
+            else{
+                imgTile.classList.add('vertical')
+            }
+            imgTile.style.background = `url(../Assets/Dungeon_SpriteSheet_Split/${img}) no-repeat center`;
+            imgTile.style.backgroundSize = "cover";
+            imgTile.style.left = `${x}px`
+            imgTile.style.top = `${y}px`
+            mazeLayerTwo.appendChild(imgTile);
+        })
     }
 }
 
