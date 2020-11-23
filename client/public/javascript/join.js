58f283d0f6f5e4745cb1d4450502be0d83497624
Faizzann <71325258+Faizzann@users.noreply.github.com>
2020-11-23 14:38:55 +0400

    Update join.js
    commented

--- client/public/javascript/join.js
+++ client/public/javascript/join.js
@@ -1,8 +1,10 @@
+//constants to store selected elements/queries 
 const createBtn = document.querySelector('.create-room-btn')
 const joinBtn = document.querySelector('.join-room-btn')
 const username = document.getElementById('username')
 const room = document.getElementById('room')
 
+//function, to be implemented by socket handler
 joinBtn.addEventListener('click', ()=>{
    
 })
