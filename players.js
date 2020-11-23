970b66597d6abcf4b13edc0c8b382649d298539e
syedayman <71809180+syedayman@users.noreply.github.com>
2020-11-23 21:09:28 +0400

    Add files via upload

---
+++ players.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const playersSchema = new Schema(
+  {
+    username:{
+        type: String,
+        required: true
+    },
+    roomId:{
+        type: String,
+        required: true
+    }
+  }
+);
+
+const Players = mongoose.model("Players", playersSchema);
+
+module.exports = Players;
