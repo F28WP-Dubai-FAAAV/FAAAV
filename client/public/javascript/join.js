9ef4527e2250e86fc56700ff8f4620e81080683f
Faizzann <71325258+Faizzann@users.noreply.github.com>
2020-11-23 14:36:06 +0400

    Add files via upload
    join.js (script for the join page)

---
+++ client/public/javascript/join.js
@@ -0,0 +1,8 @@
+const createBtn = document.querySelector('.create-room-btn')
+const joinBtn = document.querySelector('.join-room-btn')
+const username = document.getElementById('username')
+const room = document.getElementById('room')
+
+joinBtn.addEventListener('click', ()=>{
+   
+})
